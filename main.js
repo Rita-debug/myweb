d53d2541f0d6ec67ac36af3a8fb0c1be6638155d
Rita-debug <83163292+Rita-debug@users.noreply.github.com>
2022-10-23 13:28:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+ var i=1;
+function submit(){
+   
+  if (i<=4) {
+    text=document.getElementById("ip").value;
+    document.getElementById("t"+i).innerHTML=text;
+    document.getElementById("ip").value=null;
+    i++;
+  } 
+  else{
+    i=1;
+  }
+}
+
+function add()
+{
+    
+    search = document.getElementById("s1").value;
+    
+   searchitem= "https://www.google.com/search?q="+search;
+    
+    window.location = searchitem
+}
